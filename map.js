cfd9c7e8955a8263de74ad7031633c43d5ddf2c7
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-04 21:13:57 +0300

    21.13

--- map.js
+++ map.js
@@ -14,3 +14,25 @@ function multiply(value, index, arr) {
 
 console.log(doubled);
 console.log(multipled);
+
+const products = [
+    {
+        name: 'laptop',
+        price: 1000,
+        count: 5
+    },
+    {
+        name: 'desktop',
+        price: 1500,
+        count: 6
+    },
+    {
+        name: 'phone',
+        price: '500',
+        count: 7
+    }
+]
+
+const totalProductsValue = products.map(item => item.price * item.count);
+
+console.log(totalProductsValue);
