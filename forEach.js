25e637056ab58e195e98537c7b7be7773492e2fc
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-04 22:34:21 +0300

    22.34

--- forEach.js
+++ forEach.js
@@ -1,27 +1,43 @@
 const letters = [1, 2, 3, 4, 5];
 
-//letters.forEach(consoleItem);
+letters.forEach(consoleItem);
 
-// function consoleItem(item) {
-//     console.log(item)
-// }
+function consoleItem(item) {
+    console.log(item)
+}
+/*
+1
+2
+3
+4
+5
+*/
 
 
 // function consoleItem(item, index) {
 //     console.log('a[' + index + '] = ' + item);
 // }
+/*
+a[0] = 1
+a[1] = 2
+a[2] = 3
+a[3] = 4
+a[4] = 5
+*/
+
+
 
 // letters.forEach((item, index, arr) => {
 //     console.log(arr);
 // });
 
 
-let sum = 0;
-letters.forEach(item => {
-    sum += item;
-});
-console.log(sum);
-
+// let sum = 0;
+// letters.forEach(item => {
+//     sum += item;
+// });
+// console.log(sum);
+//15
 
 
 
