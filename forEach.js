9bb54099bdb7a84d1b5ae86da7270d48039752af
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-04 16:21:21 +0300

    16:20

--- forEach.js
+++ forEach.js
@@ -1,4 +1,29 @@
-const letters = ['a', 'b', 'c', 'd', 'e'];
+const letters = [1, 2, 3, 4, 5];
 
-letters.forEach()
+//letters.forEach(consoleItem);
+
+// function consoleItem(item) {
+//     console.log(item)
+// }
+
+
+// function consoleItem(item, index) {
+//     console.log('a[' + index + '] = ' + item);
+// }
+
+// letters.forEach((item, index, arr) => {
+//     console.log(arr);
+// });
+
+
+let sum = 0;
+letters.forEach(item => {
+    sum += item;
+});
+console.log(sum);
+
+
+
+
+
 
