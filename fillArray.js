5f8e554ca2c922f6314d5ada4cdcbd15cec6ee71
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-05 20:11:26 +0300

    fillArray-first

---
+++ fillArray.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+numbers.fill(0);
+console.log(numbers);
+// [ 0, 0, 0, 0, 0 ]
+
+const numbers2 = [6, 7, 8, 9, 10];
+const num2 = numbers2.fill(0,1,3);
+console.log(num2);
+// [ 6, 0, 0, 9, 10 ]
+
