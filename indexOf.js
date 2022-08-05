277eba17809d727e94b7e82b295a5d44fcd15af7
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-05 23:39:27 +0300

    indexOf.js

---
+++ indexOf.js
@@ -0,0 +1,9 @@
+//aranan elemanın bulunduğu ilk yeri gösterir
+
+const names = ['ozgur', 'baris', 'ayhan','baris'];
+console.log(names.indexOf('baris'));
+//1
+
+console.log(names.lastIndexOf('baris'));
+//3
+
