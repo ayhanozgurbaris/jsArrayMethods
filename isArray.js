a79135311f5c7693a88385a7bb8c4b27d17fb5db
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-06 00:58:19 +0300

    isArray.js

---
+++ isArray.js
@@ -0,0 +1,11 @@
+const names = ['ozgur', 'baris', 'ayhan'];
+const str = 'ozgurbarisayhan';
+const number = 17;
+console.log(Array.isArray(names));
+//true
+
+console.log(Array.isArray(str));
+//false
+
+console.log(Array.isArray(number));
+//false
