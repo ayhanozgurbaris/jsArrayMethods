f0809fb8eb916a5e93245e925eb7f8119cec3d89
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-08-05 19:58:52 +0300

    concat-mody

--- concat.js
+++ concat.js
@@ -4,3 +4,9 @@ const b = [4, 5, 6];
 const c = a.concat(b)
 console.log(c);
 // [ 1, 2, 3, 4, 5, 6 ]
+
+const d = [7, 8, 9];
+const e = a.concat(b, d);
+console.log(e);
+// [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
